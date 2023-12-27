b5f68070510e809731e9c71e37f8da20683df907
GregoryRobertson <geefromla@gmail.com>
2023-12-27 16:37:39 -0600

    updated script.js file

--- script.js
+++ script.js
@@ -1,2 +1,106 @@
+"use strict";
 
+// Define HTML elements
+const board = document.getElementById("game-board");
+const instructionText = document.getElementById('instruction-text');
+const logo
 
+// Define game variables
+const gridSize = 20;
+let snake = [{ x: 10, y: 10 }];
+let food = generateFood();
+let direction = "right";
+let gameInterval;
+let ganeSpeeDelay = 200;
+let gameStarted = false;
+
+// Draw game map, snake, food
+function draw() {
+  board.innerHTML = "";
+  drawSnake();
+  drawFood();
+}
+
+// Draw snake
+function drawSnake() {
+  snake.forEach((segment) => {
+    const snakeElement = createGameElement("div", "snake");
+    setPostion(snakeElement, segment);
+    board.appendChild(snakeElement);
+  });
+}
+
+// Create a snake or food cube/div
+function createGameElement(tag, className) {
+  const element = document.createElement(tag);
+  element.className = className;
+  return element;
+}
+
+// Set the position of the snake or food
+function setPostion(element, position) {
+  element.style.gridColumn = position.x;
+  element.style.gridRow = position.y;
+}
+
+// Testing draw function
+draw();
+
+//Draw food function
+function drawFood() {
+  const foodElement = createGameElement("div", "food");
+  setPostion(foodElement, food);
+  board.appendChild(foodElement);
+}
+
+// Generate food
+function generateFood() {
+  const x = Math.floor(Math.random() * gridSize) + 1;
+  const y = Math.floor(Math.random() * gridSize) + 1;
+  return { x, y };
+}
+
+// Moving the snake
+function move() {
+  const head = { ...snake[0] };
+  switch (direction) {
+    case "up":
+      head.y--;
+      break;
+    case "down++":
+      head.x++;
+      break;
+    case "left":
+      head.x--;
+      break;
+    case "right":
+      head.x++;
+      break;
+  }
+  snake.unshift(head);
+
+  // snake.pop();
+
+  if ((head.x = food.x && head.y == food.y)) {
+    food = generateFood();
+    clearInterval(); // Clear past interval
+    gameInterval = setInterval(() => {
+      move();
+      draw();
+    }, gameSpeedDelay);
+  } else {
+    snake.pop();
+  }
+}
+
+// Test moving
+// setInterval(() => {
+//  move(); // Move first
+// draw(); // Then draw again new position
+// }, 200)
+
+//Start game function
+function startGame() {
+  gameStarted = true; // Keep track of a running game
+  instruction
+}
